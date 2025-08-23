b23f782bc4bb9b68ecf1d88ee17221fc833f156f
werlensantos <werlencarsts@gmail.com>
2025-08-23 19:07:49 -0300

    chore: register services in provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use App\Repositories\Contracts\UserRepositoryInterface;
 use App\Repositories\Eloquent\EloquentUserRepository;
+use App\Services\AuthAdminService;
+use App\Services\AuthUserService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        // Bind repositories
         $this->app->bind(UserRepositoryInterface::class, EloquentUserRepository::class);
+
+        // Bind services of authentication
+        $this->app->bind(AuthAdminService::class, function ($app) {
+            return new AuthAdminService($app->make(UserRepositoryInterface::class));
+        });
+
+        $this->app->bind(AuthUserService::class, function ($app) {
+            return new AuthUserService($app->make(UserRepositoryInterface::class));
+        });
     }
 
     /**
