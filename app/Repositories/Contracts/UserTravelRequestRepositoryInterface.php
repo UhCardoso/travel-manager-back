508edbd74ea738cc9b78db70b9727428f7e7771b
werlensantos <werlencarsts@gmail.com>
2025-08-24 10:44:39 -0300

    feat: user repository interface

--- app/Repositories/Contracts/UserTravelRequestRepositoryInterface.php
+++ app/Repositories/Contracts/UserTravelRequestRepositoryInterface.php
@@ -3,6 +3,7 @@
 namespace App\Repositories\Contracts;
 
 use App\Models\TravelRequest;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
 interface UserTravelRequestRepositoryInterface
 {
@@ -12,7 +13,18 @@ interface UserTravelRequestRepositoryInterface
     public function store(array $data): TravelRequest;
 
     /**
+     * Get all travel requests with pagination
+     */
+    public function getAll(int $userId, array $params): LengthAwarePaginator;
+
+    /**
      * Get the details of a travel request
      */
     public function getDetails(int $userId, int $travelRequestId): ?TravelRequest;
+
+    /**
+     * Cancel a travel request
+     * User can only cancel if not approved
+     */
+    public function cancel(int $userId, int $travelRequestId): TravelRequest;
 }
