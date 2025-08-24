e87b901c57d82011b11063d3bfbff009058dee5f
werlensantos <werlencarsts@gmail.com>
2025-08-24 10:44:10 -0300

    chore: bind servicesm, repositories and observer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,12 +2,18 @@
 
 namespace App\Providers;
 
+use App\Models\TravelRequest;
+use App\Observers\TravelRequestObserver;
+use App\Repositories\Contracts\AdminTravelRequestRepositoryInterface;
 use App\Repositories\Contracts\UserRepositoryInterface;
 use App\Repositories\Contracts\UserTravelRequestRepositoryInterface;
+use App\Repositories\Eloquent\AdminTravelRequestRepository;
 use App\Repositories\Eloquent\EloquentUserRepository;
 use App\Repositories\Eloquent\UserTravelRequestRepository;
+use App\Services\AdminTravelRequestService;
 use App\Services\AuthAdminService;
 use App\Services\AuthUserService;
+use App\Services\UserTravelRequestService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
         // Bind repositories
         $this->app->bind(UserRepositoryInterface::class, EloquentUserRepository::class);
         $this->app->bind(UserTravelRequestRepositoryInterface::class, UserTravelRequestRepository::class);
+        $this->app->bind(AdminTravelRequestRepositoryInterface::class, AdminTravelRequestRepository::class);
 
         // Bind services of authentication
         $this->app->bind(AuthAdminService::class, function ($app) {
@@ -29,6 +36,16 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(AuthUserService::class, function ($app) {
             return new AuthUserService($app->make(UserRepositoryInterface::class));
         });
+
+        // Bind admin travel request service
+        $this->app->bind(AdminTravelRequestService::class, function ($app) {
+            return new AdminTravelRequestService($app->make(AdminTravelRequestRepositoryInterface::class));
+        });
+
+        // Bind user travel request service
+        $this->app->bind(UserTravelRequestService::class, function ($app) {
+            return new UserTravelRequestService($app->make(UserTravelRequestRepositoryInterface::class), $app->make(UserRepositoryInterface::class));
+        });
     }
 
     /**
@@ -36,6 +53,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Register observers
+        TravelRequest::observe(TravelRequestObserver::class);
     }
 }
